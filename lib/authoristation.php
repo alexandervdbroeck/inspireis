6cda634f740c2e18b9ba30c989053dbe7c316d03
alexandervdbroeck <alexander.vdbroeck@gmail.com>
2019-12-03 19:55:42 +0100

    herstellen log in modula na het verwijderen van de mailfunctie

--- lib/authoristation.php
+++ lib/authoristation.php
@@ -4,21 +4,14 @@ require_once "autoload.php";
 function StartLoginSession($login, $paswd )
 {
     //gebruiker opzoeken ahv zijn login (e-mail)
-    $sql = "SELECT * FROM user WHERE usr_email='" . $login . "' ";
+    $sql = "SELECT * FROM users WHERE usr_email='" . $login . "' ";
     $data = GetData($sql);
     if ( count($data) == 1 )
     {
         $row = $data[0];
-        if ($row['usr_admin'] == 0) {
-            session_start();
-            $_SESSION['message'] = "uw account is nog niet geactiveerd";
-            return false;
-        }
         //password controleren
-        if ( password_verify( $paswd, $row['usr_paswoord'] ))  $login_ok = true;
-
+        if ( password_verify( $paswd, $row['usr_paswoord'] ) ) $login_ok = true;
     }
-
 
     if ( $login_ok )
     {
@@ -26,10 +19,7 @@ function StartLoginSession($login, $paswd )
         $_SESSION['usr'] = $row;
         return true;
     }
-    $_SESSION['message'] = "uw e-mail of paswoord is niet correct";
+
     return false;
 }
-
-
-
 
