bf594bfc026bfbaa77e29d5234fff929edaff542
alexandervdbroeck <alexander.vdbroeck@gmail.com>
2019-12-04 17:09:36 +0100

    creeerpost basis functies klaar : =>invoeren van blogtekst (zonder reformatting) =>zonder foto upload

--- creeer.php
+++ creeer.php
@@ -3,6 +3,8 @@ require_once "lib/autoload.php";
 PrintPageSection("head");
 PrintPageSection("nav");
 if(isset($_SESSION["message"])) print $_SESSION["message"];
+print $_SESSION['blogtext'];
+unset($_SESSION['blogtext']);
 unset($_SESSION["message"])
 ?>
 <main class="container ">
--- lib/creeerpost.php
+++ lib/creeerpost.php
@@ -7,6 +7,7 @@ $tablename = $_POST["tablename"];
 unset($_SESSION['message']);
 // controlo of het juiste formulier aankomt
 if ($formname == "creeer_form" AND $_POST['submitpost'] == "save_post") {
+    var_dump($_POST);
 
     // sql statement samenstellen
     $sql = "INSERT INTO $tablename SET " .
@@ -16,15 +17,15 @@ if ($formname == "creeer_form" AND $_POST['submitpost'] == "save_post") {
         " post_user_id='" . $_SESSION['usr']['usr_id'] . "' , " .
         " post_cat_id='" . $_POST['cat_naam'] . "', ".
         " post_land_id='" . $_POST['land_id'] . "' ";
-    var_dump($sql);
-    if (ExecuteSQL($sql)){
-        $_SESSION['message']= "uw blog is gepost";
+    // Post in database opslaan
+  //  if (ExecuteSQL($sql)){
+    //    $_SESSION['message']= "uw blog is gepost";
+     //   $_SESSION['blogtext']= $_POST['post_blog'];
+       // header ("location:../creeer.php");
+//    }else {
+  //      $_SESSION['message']= "er liep iets mis";
         var_dump($_SESSION);
-        //header ("location:../creeer.php");
-    }else {
-        $_SESSION['message']= "er liep iets mis";
-        var_dump($_SESSION);
-        //header ("location:../creeer.php");
-    }
+       // header ("location:../creeer.php");
+  //  }
 
 }
