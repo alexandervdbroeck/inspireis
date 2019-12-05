983681af6e5e8404c9abfd268c9f5977b3743dbe
alexandervdbroeck <alexander.vdbroeck@gmail.com>
2019-12-05 11:56:22 +0100

    foto's naar de database schrijven. => foto's worden naar database geschreven met hun respectievelijk directory => nog geen controle op blog text of foto

--- lib/creeerpost.php
+++ lib/creeerpost.php
@@ -6,7 +6,7 @@ $formname = $_POST["formname"];
 $tablename = $_POST["tablename"];
 $user_id = $_SESSION['usr']['usr_id'];
 unset($_SESSION['message']);
-// controlo of het juiste formulier aankomt
+// controle of het juiste formulier aankomt
 if ($formname == "creeer_form" AND $_POST['submitpost'] == "save_post") {
     // sql statement samenstellen
     $sql = "INSERT INTO $tablename SET " .
@@ -25,29 +25,45 @@ if ($formname == "creeer_form" AND $_POST['submitpost'] == "save_post") {
         $_SESSION['message']= "er liep iets mis";
        // header ("location:../creeer.php");
     }
-    // afbeelding uploaden, eerst de post_id bekomen
+    // om de afbeelding een naam te geven, eerst de net gecreeerde post_id ophalen
     $sql = "SELECT post_id FROM post WHERE post_user_id ='".$user_id."' 
                                     order by post_datum desc limit 1" ;
     $post_id = GetDataOneRow($sql );
     $post_id = $post_id['post_id'];
 
-    //directory samenstellen om foto's in op te slaan (images/user_XX/)
+    //directory samenstellen om foto's in op te slaan (images/user_XX/) en als deze niet bestaad aanmaken
+
     if(!is_dir("../images/user_".$user_id))mkdir("../images/user_".$user_id);
     $target_dir = "../images/user_".$user_id."/";
+
+    // controleren hoeveel foto's er toegevoegd moeten worden
     $countfiles = count($_FILES["filename"]["name"]);
+
+    // een lijst aanmaken om de namen van de foto's in op te slaan
+    $fotos = array();
 
     // files stuk voor stuk downloaden
     for($i=0;$i<$countfiles;$i++){
-        // file herbenoemen(postid_en nr foto
-        $filename = $_FILES["filename"]["name"][$i];
+        // foto herbenoemen naar (postid_fotonr) in lowercase letters
+        $filename = strtolower($_FILES["filename"]["name"][$i]) ;
         $fileExplode = explode(".",$filename);
         $fileExt = end($fileExplode);
+        //fotonr creeren aan de hand van de $i
         $fotonr = $i+1;
         $_FILES["filename"]["name"][$i] = $post_id."_".$fotonr.".".$fileExt;
-        var_dump( $_FILES["filename"]["name"][$i]);
+        // filename  aan lijst toevoegen voor later gebruik(in database invoer)
+        array_push($fotos,$_FILES["filename"]["name"][$i]);
         $target_file = $target_dir.basename($_FILES["filename"]["name"][$i]);
+        // de foto uploaden in zijn usermap
         move_uploaded_file($_FILES["filename"]["tmp_name"][$i],$target_file);
     }
 
+    // fotos in database zetten aan de hand
+
+    for($i=0;$i<$countfiles;$i++){
+        $sql = "INSERT INTO afbeelding SET afb_post_id =". $post_id.",afb_filename='".$fotos[$i]."', afb_locatie= 'images/user_".$user_id."/".$fotos[$i]."'";
+        ExecuteSQL($sql);
+
+    }
 
 }
