3d5051742f7f1c997689186fd12cace04fb3262c
alexandervdbroeck <alexander.vdbroeck@gmail.com>
2019-12-10 13:56:33 +0100

    database

---
+++ lib/connection.php
@@ -0,0 +1,10 @@
+<?php
+function GetConnectionData()
+{
+    return array(
+        "dsn" => "mysql:host=185.115.218.166;dbname=wdev_alexander",
+        "user" => "wdev_alexander",
+        "passwd" => "u2k8EwwQvDav" ) ;
+}
+?>
+
